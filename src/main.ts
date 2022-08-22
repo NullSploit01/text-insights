0f09a4353e6933167f9fbc6c544cb535d45a2a46
Harshal <harshaldharmik099@gmail.com>
2022-08-23 01:10:31 +0530

    Fixed asic issues

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
+import { Quasar } from "quasar";
+import quasarUserOptions from "./quasar-user-options";
 
-const app = createApp(App)
+const app = createApp(App).use(Quasar, quasarUserOptions);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
